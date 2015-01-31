8f80100c8d9a3966fbc91d983c798477db868ea5
NishantPanchal92 <nishpanchal21@gmail.com>
2015-01-31 16:45:15 +0530

    Update JS_ruby.rb

--- JS in ruby/JS_ruby.rb
+++ JS in ruby/JS_ruby.rb
@@ -22,50 +22,69 @@ uri=URI.parse("http://api.browserstack.com/3/browsers")
 request=Net::HTTP::Get.new(uri.request_uri)
 request.basic_auth("<USER_NAME>","<AUTOMATE_KEY>") 
 response=http.request(request)
-#puts response.code
 puts response.body
-#parsedJson= JSON.parse(response.body)
 
-#To fetch the json data
-# data=parsedJson["Windows"]["8.1"]
-# data.each do |data|
-# 	pp data["browser_version"]
-# end
+## To fetch particular data from the JSON retrieved ##
+parsedJson= JSON.parse(response.body)
+data=parsedJson["Windows"]["8.1"]
+data.each do |data|
+  pp data["browser_version"]
+end
 
 #Create a worker
 uri=URI.parse("http://api.browserstack.com/3/worker")
 request=Net::HTTP::Post.new(uri.request_uri)
 request.initialize_http_header({"Content-Type" => "application/json", "Accept" => "application/json"})
 request.basic_auth("<USER_NAME>","<AUTOMATE_KEY>") 
-request.set_form_data("os" => "Windows", "os_version" => "7", "browser_version" => "8.0", "browser" => "ie", "url" => "http://google.com")
+
+## Capabilities for running tests on desktop browsers and mobile devices  ##
+request.set_form_data("os" => "OS X", "os_version" => "Mavericks", "browser_version" => "7", "browser" => "safari", "url" => "http://localhost", "build" => "Local JS", "browserstack.local" => "true")
+
+## Capabilities for running tests on iOS simulators ##
+# request.set_form_data("build" => "ios test", "real_mobile" => 'false', "device" => "iPhone 5S", "os" => "ios", "os_version" => "7.0", "browser_version" => "null", "browser" => "Mobile Safari", "url" => "http://localhost")
+
 response=http.request(request)
-#puts response.code
-#puts response.body
 jobId= JSON.parse(response.body)["id"]
 puts "http://api.browserstack.com/3/worker/#{jobId}"
 
-#Get job status
+## Get job status. The script will continue only after it receives the "running" status ##
+puts "Job Status"
 uri=URI.parse("http://api.browserstack.com/3/worker/#{jobId}")
 request=Net::HTTP::Get.new(uri.request_uri)
-request.basic_auth("<USER_NAME>","<AUTOMATE_KEY>") 
+request.basic_auth("nishant57","S7ZUamAuUp24pPLhDYsi")
+response=http.request(request)
+status = JSON.parse(response.body)["status"]
+puts status
+while status!="running"
+response = http.request(request)
+status = JSON.parse(response.body)["status"]
+puts status
+end
+
+## Capture Screenshot ##
+puts "Screenshot status"
+uri=URI.parse("http://api.browserstack.com/3/worker/#{jobId}/screenshot.json")
+request=Net::HTTP::Get.new(uri.request_uri)
+request.basic_auth("nishant57","S7ZUamAuUp24pPLhDYsi")
 response=http.request(request)
 puts response.body
+puts "screenshot captured"
 
-#Get all the workers
+## Get all active workers ##
 uri=URI.parse("http://api.browserstack.com/3/workers")
 request=Net::HTTP::Get.new(uri.request_uri)
 request.basic_auth("<USER_NAME>","<AUTOMATE_KEY>") 
 response=http.request(request)
 puts response.body
 
-#Get API status
+## Get API status ##
 uri=URI.parse("http://api.browserstack.com/3/status")
 request=Net::HTTP::Get.new(uri.request_uri)
 request.basic_auth("<USER_NAME>","<AUTOMATE_KEY>") 
 response=http.request(request)
 puts response.body
 
-#Delete worker 
+## Delete worker ##
 uri=URI.parse("http://api.browserstack.com/3/worker/#{jobId}")
 request=Net::HTTP::Delete.new(uri.request_uri)
 request.basic_auth("<USER_NAME>","<AUTOMATE_KEY>") 
